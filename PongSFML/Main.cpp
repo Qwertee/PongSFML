48e80edb3e847910a6359e63b6fbd0b0e663c982
Qwertee <mobiusdragon96@gmail.com>
2016-09-30 01:01:18 -0500

    Added win conditions and game now resets when victory condition is met.

--- PongSFML/Main.cpp
+++ PongSFML/Main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
-#include <thread>
+#include "thread"
+#include <stdlib.h>
 // --------------
 #include <SFML/Graphics.hpp>
 
@@ -18,6 +19,8 @@
 // May or may not be used at this point.
 using namespace std::chrono;
 
+void resetGame(sf::RectangleShape& ball);
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "PONG");
@@ -31,6 +34,9 @@ int main()
 	float ballVelocityX = BALL_VELOCITY;
 	float ballVelocityY = BALL_VELOCITY;
 
+	unsigned int scorePlayer1 = 0;
+	unsigned int scorePlayer2 = 0;
+
 	// The paddles
 	sf::RectangleShape player1(sf::Vector2f(PADDLE_WIDTH, PADDLE_HEIGHT));
 	player1.setFillColor(sf::Color::White);
@@ -41,12 +47,10 @@ int main()
 	sf::FloatRect player1BoundingBox;
 	sf::FloatRect player2BoundingBox;
 
-	
-	/*
+	bool reset = false;
 
-	-----MainLoop----- TODO: add an update and draw method to make this prettier
-	
-	*/
+
+	// -----MainLoop----- TODO: add an update and draw method to make this prettier
 
 	// time_point<system_clock> t;
 	while (window.isOpen())
@@ -61,19 +65,23 @@ int main()
 
 		//update everything
 		// player movement
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
 			player1.move(0, -PADDLE_VELOCITY);
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
 			player1.move(0, PADDLE_VELOCITY);
 		}
 
 		// Computer paddle movement
-		if (player2.getPosition().y + (PADDLE_HEIGHT / 2) < ball.getPosition().y) {
+		if (player2.getPosition().y + (PADDLE_HEIGHT / 2) < ball.getPosition().y)
+		{
 			// move the paddle DOWN
 			player2.move(0, PADDLE_VELOCITY);
 		}
-		if (player2.getPosition().y + (PADDLE_HEIGHT / 2) > ball.getPosition().y) {
+		if (player2.getPosition().y + (PADDLE_HEIGHT / 2) > ball.getPosition().y)
+		{
 			// move the paddle UP
 			player2.move(0, -PADDLE_VELOCITY);
 		}
@@ -88,14 +96,32 @@ int main()
 		player2BoundingBox = player2.getGlobalBounds();
 
 		// check if the ball has touched either the top or bottom of the display
-		if (ball.getPosition().y < 0 || ball.getPosition().y > WINDOW_HEIGHT) {
+		if (ball.getPosition().y < 0 || ball.getPosition().y > WINDOW_HEIGHT)
+		{
 			ballVelocityY = -ballVelocityY;
 		}
 
 		// now check if any of the BoundingBoxes are intersecting and perform 
 		// the necessary action(s) if they are.
-		if (ballBoundingBox.intersects(player1BoundingBox) || ballBoundingBox.intersects(player2BoundingBox)) {
+		if (ballBoundingBox.intersects(player1BoundingBox) || ballBoundingBox.intersects(player2BoundingBox))
+		{
 			ballVelocityX = -ballVelocityX;
+		}
+
+		// TODO: Check for victory condition here and keep track of the scores
+
+		// Check if player won
+		if (ball.getPosition().x >= WINDOW_WIDTH)
+		{
+			scorePlayer1++;
+			
+		}
+
+		// Check if computer won
+		if (ball.getPosition().x <= 0)
+		{
+			scorePlayer2++;
+			reset = true;
 		}
 
 		// draw everything
@@ -105,9 +131,21 @@ int main()
 		window.draw(player2);
 		window.display();
 
+
+		if (reset)
+		{
+			resetGame(ball);
+			reset = false;
+		}
 		// t += milliseconds(33);
 		// std::this_thread::sleep_until(t);
 	}
 
 	return 0;
+}
+
+void resetGame(sf::RectangleShape& ball)
+{
+	sf::sleep(sf::seconds(2));
+	ball.setPosition(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
 }
